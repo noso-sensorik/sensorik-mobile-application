e6fd237feaade6e8094f3287cb7385832e233b83
Dana Engel <engel.dana.mail@gmail.com>
2019-03-13 15:29:20 +0100

    add first draft

---
+++ app/src/main/java/ch/bfh/ti/noso_sensorik/sensorik_mobile_application/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.bfh.ti.noso_sensorik.sensorik_mobile_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
